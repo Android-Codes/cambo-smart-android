eefdcdcb8830ce623dec4c960e2d78f39754f954
Chivon <chhaichivon@2ntkh.com>
2017-04-27 07:33:50 +0700

    add model

---
+++ app/src/main/java/com/nanita/cambosmart/models/User.java
@@ -0,0 +1,13 @@
+package com.nanita.cambosmart.models;
+
+/**
+ * Created by chhaichivon on 4/27/17.
+ */
+
+public class User {
+    private int id;
+    private String username;
+    private String email;
+    private String password;
+    private String role;
+}
